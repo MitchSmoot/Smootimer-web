7a0097a0672d28c8a6fb9526562915767cd4fa8c
Mitch Smoot <mitchsmoot@gmail.com>
2019-03-19 02:24:10 -0700

    enabled google authentication

--- src/app/app-nav/app-nav.component.ts
+++ src/app/app-nav/app-nav.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './app-nav.component.html',
   styleUrls: ['./app-nav.component.scss']
 })
-export class ApplicationNavComponent {}
+export class ApplicationNavComponent {
+  constructor(
+    public fireAuth:AngularFireAuth
+  ) {
+
+  }
+
+  login() {
+    this.fireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.fireAuth.auth.signOut();
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,19 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   items: Observable<any[]>;
-  constructor(db: AngularFirestore) {
+  constructor(
+    db: AngularFirestore,
+    public fireAuth:AngularFireAuth
+    ) {
     this.items = db.collection('items').valueChanges();
   }
   title = 'smootimer-web';
+
+  login() {
+    this.fireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.fireAuth.auth.signOut();
+  }
 }
