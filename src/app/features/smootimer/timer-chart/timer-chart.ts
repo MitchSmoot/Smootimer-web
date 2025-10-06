1afb5749179ee6ed957a5f62d0ab5edaabea290e
MitchSmoot <mitchsmoot@gmail.com>
2025-10-06 15:26:59 -0600

    basic chart functionality with signal driven data

--- src/app/features/smootimer/timer-chart/timer-chart.ts
+++ src/app/features/smootimer/timer-chart/timer-chart.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import * as d3 from 'd3';
-import { TimerService } from '../timer/timer.service';
+import { Solve, TimerService } from '../timer/timer.service';
 
 @Component({
   selector: 'app-timer-chart',
@@ -9,29 +9,68 @@ import { TimerService } from '../timer/timer.service';
   styleUrl: './timer-chart.scss'
 })
 export class TimerChart {
-  timerService = inject(TimerService);
-  solves = this.timerService.solves;
+  private timerService = inject(TimerService);
+  solves = this.timerService.solves();
 
 
-  ngOnChanges(): void {
-    this.createChart();
+  private svg: d3.Selection<SVGGElement, unknown, HTMLElement, any> | null = null;
+  private width = 600;
+  private height = 400;
+  private margin = { top: 20, right: 20, bottom: 30, left: 50 };
+
+  constructor() {
+    // Effect to react to data changes
+    effect(() => {
+      const solves = this.timerService.solves();
+      this.updateChart(solves);
+    });
   }
 
-  createChart(): void {
-   const data = this.solves().map(solve => solve.time);
-   const svg = d3.select("app-timer-chart").append("svg")
-                 .attr("width", 700)
-                 .attr("height", 300);
-   
-   svg.selectAll("rect")
-     .data(data)
-     .enter()
-     .append("rect")
-     .attr("x", (d, i) => i * 70)
-     .attr("y", d => 300 - d)
-     .attr("width", 65)
-     .attr("height", d => d)
-     .attr("fill", "blue");
- }
+  private updateChart(solves: Solve[]): void {
+    // Clear existing chart
+    d3.select('#chart').selectAll('*').remove();
+
+    // Set up SVG container
+    this.svg = d3.select('#chart')
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .append('g')
+      .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
+
+    const innerWidth = this.width - this.margin.left - this.margin.right;
+    const innerHeight = this.height - this.margin.top - this.margin.bottom;
+
+    // Set up scales
+    const x = d3.scaleTime()
+      .domain(d3.extent(solves, d => d.solveDate) as [Date, Date])
+      .range([0, innerWidth]);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(solves, d => d.time) as number])
+      .range([innerHeight, 0]);
+
+    // Define the line
+    const line = d3.line<{ solveDate: Date, time: number }>()
+      .x(d => x(d.solveDate))
+      .y(d => y(d.time));
+
+    // Add the line path
+    this.svg.append('path')
+      .datum(solves)
+      .attr('fill', 'none')
+      .attr('stroke', 'steelblue')
+      .attr('stroke-width', 2)
+      .attr('d', line);
+
+    // Add x-axis
+    this.svg.append('g')
+      .attr('transform', `translate(0,${innerHeight})`)
+      .call(d3.axisBottom(x));
+
+    // Add y-axis
+    this.svg.append('g')
+      .call(d3.axisLeft(y));
+  }
 
 }
